5deafe2bc90e206201bd3ce49517c4e0fa95cc8d
Ajs07 <arnoldjulian24@gmail.com>
2024-06-25 19:08:26 +0700

    upload code perpindahan data bagian awal

--- App.js
+++ App.js
@@ -5,59 +5,29 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 // melakukan perpindahan halaman
-// const HomeScreen = ({ navigation }) => {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Home Screen</Text>
-//       <Button
-//         title="Pergi ke Progate"
-//         onPress={() => navigation.navigate('Progate')}
-//       />
-//     </View>
-//   )
-// }
+ const HomeScreen = ({ navigation }) => {
+   return (
+     <View style={styles.container}>
+       <Text>Home Screen</Text>
+       <Button
+         title="Pergi ke Progate"
+         onPress={() => navigation.navigate('Progate')}
+       />
+     </View>
+   )
+ }
 
-// melakukan perpindahan data antar component
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Pergi ke Progate"
-        // untuk pengiriman data masih menggunakan navigation.navigate() tapi ada tambahan argument
-        onPress={() => {
-          // Argument ke-1 adalah untuk nama halaman yang akan dituju dan argument yang ke-2 adalah untuk data yang akan kita kirim 
-          navigation.navigate('Progate', {
-            name: 'Ninja Ken',
-            language: 'React Native',
-          })
-        }}
-      />
-    </View>
-  )
-}
 
 // perpindahan data antar halaman
-// const ProgateScreen = ({ navigation }) => {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Welcome to Progate!</Text>
-//       <Button title="Kembali" onPress={() => navigation.goBack()} />
-//     </View>
-//   )
-// }
+ const ProgateScreen = ({ navigation }) => {
+   return (
+     <View style={styles.container}>
+       <Text>Welcome to Progate!</Text>
+       <Button title="Kembali" onPress={() => navigation.goBack()} />
+     </View>
+   )
+ }
 
-// perpindahan data dalam component
-const ProgateScreen = ({ route, navigation }) => {
-  return (
-    // Data yang terkirim dari layar sebelumnya bisa diakses melalui route.params
-    <View style={styles.container}>
-      <Text>Welcome to Progate, {route.params.name}!</Text>
-      <Text>Ayo belajar {route.params.language}!</Text>
-      <Button title="Kembali" onPress={() => navigation.goBack()} />
-    </View>
-  )
-}
 
 // Membuat NativeStackNavigator dan menetapkanya ke sebuah constant yang diberi nama Stack
 const Stack = createNativeStackNavigator()
